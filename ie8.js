b0c7c1d454c315d6a84ec0ebfe5d8133533f91ab
Alex Mattrick <itaieps+github@fastmail.fm>
2015-06-19 08:15:45 -0400

    Create ie8.js

---
+++ ie8.js
@@ -0,0 +1,644 @@
+(function(){
+
+  function isFunc(obj) {
+    return (typeof obj === 'function');
+  }
+
+  if (!isFunc(Object.create)) {
+    Object.create = (function() {
+      function Temp() {}
+
+      var hasOwn = Object.prototype.hasOwnProperty;
+
+      return function (O) {
+        if (typeof O != 'object') {
+          throw TypeError('Object prototype may only be an Object or null');
+        }
+
+        Temp.prototype = O;
+        var obj = new Temp();
+        Temp.prototype = null;
+
+        if (arguments.length > 1) {
+          var Properties = Object(arguments[1]);
+          for (var prop in Properties) {
+            if (hasOwn.call(Properties, prop)) {
+              obj[prop] = Properties[prop];
+            }
+          }
+        }
+
+        return obj;
+      };
+    })();
+  }
+
+  if (!isFunc(Object.defineProperties)) {
+    Object.defineProperties = function defineProperties(obj, properties) {
+      function convertToDescriptor(desc) {
+        function hasProperty(obj, prop) {
+          return Object.prototype.hasOwnProperty.call(obj, prop);
+        }
+        function isCallable(v) {
+          return typeof v === "function";
+        }
+        if (typeof desc !== "object" || desc === null)
+          throw new TypeError("bad desc");
+
+        var d = {};
+
+        if (hasProperty(desc, "enumerable"))
+          d.enumerable = !!obj.enumerable;
+        if (hasProperty(desc, "configurable"))
+          d.configurable = !!obj.configurable;
+        if (hasProperty(desc, "value"))
+          d.value = obj.value;
+        if (hasProperty(desc, "writable"))
+          d.writable = !!desc.writable;
+        if (hasProperty(desc, "get")) {
+          var g = desc.get;
+
+          if (!isCallable(g) && typeof g !== "undefined")
+            throw new TypeError("bad get");
+          d.get = g;
+        }
+        if (hasProperty(desc, "set")) {
+          var s = desc.set;
+          if (!isCallable(s) && typeof s !== "undefined")
+            throw new TypeError("bad set");
+          d.set = s;
+        }
+
+        if (("get" in d || "set" in d) && ("value" in d || "writable" in d))
+          throw new TypeError("identity-confused descriptor");
+
+        return d;
+      }
+
+      if (typeof obj !== "object" || obj === null)
+        throw new TypeError("bad obj");
+
+      properties = Object(properties);
+
+      var keys = Object.keys(properties);
+      var descs = [];
+
+      for (var i = 0; i < keys.length; i++)
+        descs.push([keys[i], convertToDescriptor(properties[keys[i]])]);
+
+      for (var i = 0; i < descs.length; i++)
+        Object.defineProperty(obj, descs[i][0], descs[i][1]);
+
+      return obj;
+    };
+  }
+
+  // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+  if (!Object.keys) {
+    Object.keys = (function() {
+      'use strict';
+      var hasOwnProperty = Object.prototype.hasOwnProperty,
+          hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+          dontEnums = [
+            'toString',
+            'toLocaleString',
+            'valueOf',
+            'hasOwnProperty',
+            'isPrototypeOf',
+            'propertyIsEnumerable',
+            'constructor'
+          ],
+          dontEnumsLength = dontEnums.length;
+
+      return function(obj) {
+        if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+          throw new TypeError('Object.keys called on non-object');
+        }
+
+        var result = [], prop, i;
+
+        for (prop in obj) {
+          if (hasOwnProperty.call(obj, prop)) {
+            result.push(prop);
+          }
+        }
+
+        if (hasDontEnumBug) {
+          for (i = 0; i < dontEnumsLength; i++) {
+            if (hasOwnProperty.call(obj, dontEnums[i])) {
+              result.push(dontEnums[i]);
+            }
+          }
+        }
+        return result;
+      };
+    }());
+  }
+
+  if (!Date.prototype.toISOString) {
+    (function() {
+
+      function pad(number) {
+        if (number < 10) {
+          return '0' + number;
+        }
+        return number;
+      }
+
+      Date.prototype.toISOString = function() {
+        return this.getUTCFullYear() +
+          '-' + pad(this.getUTCMonth() + 1) +
+          '-' + pad(this.getUTCDate()) +
+          'T' + pad(this.getUTCHours()) +
+          ':' + pad(this.getUTCMinutes()) +
+          ':' + pad(this.getUTCSeconds()) +
+          '.' + (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5) +
+          'Z';
+      };
+
+    }());
+  }
+
+  if (!Date.now) {
+    Date.now = function now() {
+      return new Date().getTime();
+    };
+  }
+
+  if (!Array.isArray) {
+    Array.isArray = function(arg) {
+      return Object.prototype.toString.call(arg) === '[object Array]';
+    };
+  }
+
+  if (!window.JSON) {
+    window.JSON = {
+      parse: function(sJSON) { return eval('(' + sJSON + ')'); },
+      stringify: (function () {
+        var toString = Object.prototype.toString;
+        var isArray = Array.isArray || function (a) { return toString.call(a) === '[object Array]'; };
+        var escMap = {'"': '\\"', '\\': '\\\\', '\b': '\\b', '\f': '\\f', '\n': '\\n', '\r': '\\r', '\t': '\\t'};
+        var escFunc = function (m) { return escMap[m] || '\\u' + (m.charCodeAt(0) + 0x10000).toString(16).substr(1); };
+        var escRE = /[\\"\u0000-\u001F\u2028\u2029]/g;
+        return function stringify(value) {
+          if (value == null) {
+            return 'null';
+          } else if (typeof value === 'number') {
+            return isFinite(value) ? value.toString() : 'null';
+          } else if (typeof value === 'boolean') {
+            return value.toString();
+          } else if (typeof value === 'object') {
+            if (typeof value.toJSON === 'function') {
+              return stringify(value.toJSON());
+            } else if (isArray(value)) {
+              var res = '[';
+              for (var i = 0; i < value.length; i++)
+                res += (i ? ', ' : '') + stringify(value[i]);
+              return res + ']';
+            } else if (toString.call(value) === '[object Object]') {
+              var tmp = [];
+              for (var k in value) {
+                if (value.hasOwnProperty(k))
+                  tmp.push(stringify(k) + ': ' + stringify(value[k]));
+              }
+              return '{' + tmp.join(', ') + '}';
+            }
+          }
+          return '"' + value.toString().replace(escRE, escFunc) + '"';
+        };
+      })()
+    };
+  }
+
+  if (!Function.prototype.bind) {
+    Function.prototype.bind = function(oThis) {
+      if (typeof this !== 'function') {
+        // closest thing possible to the ECMAScript 5
+        // internal IsCallable function
+        throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+      }
+
+      var aArgs   = Array.prototype.slice.call(arguments, 1),
+          fToBind = this,
+          fNOP    = function() {},
+          fBound  = function() {
+            return fToBind.apply(this instanceof fNOP
+                   ? this
+                   : oThis,
+                   aArgs.concat(Array.prototype.slice.call(arguments)));
+          };
+
+      fNOP.prototype = this.prototype;
+      fBound.prototype = new fNOP();
+
+      return fBound;
+    };
+  }
+
+  if (!String.prototype.trim) {
+    (function() {
+      // Make sure we trim BOM and NBSP
+      var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+      String.prototype.trim = function() {
+        return this.replace(rtrim, '');
+      };
+    })();
+  }
+
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(searchElement, fromIndex) {
+      var k;
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var O = Object(this);
+      var len = O.length >>> 0;
+
+      if (len === 0) return -1;
+
+      var n = +fromIndex || 0;
+
+      if (Math.abs(n) === Infinity) {
+        n = 0;
+      }
+
+      if (n >= len) {
+        return -1;
+      }
+      k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      while (k < len) {
+        if (k in O && O[k] === searchElement) return k;
+        k++;
+      }
+      return -1;
+    };
+  }
+
+  if (!Array.prototype.lastIndexOf) {
+    Array.prototype.lastIndexOf = function(searchElement /*, fromIndex*/) {
+      'use strict';
+
+      if (this === void 0 || this === null) {
+        throw new TypeError();
+      }
+
+      var n, k,
+        t = Object(this),
+        len = t.length >>> 0;
+      if (len === 0) {
+        return -1;
+      }
+
+      n = len - 1;
+      if (arguments.length > 1) {
+        n = Number(arguments[1]);
+        if (n != n) {
+          n = 0;
+        }
+        else if (n != 0 && n != (1 / 0) && n != -(1 / 0)) {
+          n = (n > 0 || -1) * Math.floor(Math.abs(n));
+        }
+      }
+
+      for (k = n >= 0 ? Math.min(n, len - 1) : len - Math.abs(n); k >= 0; k--) {
+        if (k in t && t[k] === searchElement) {
+          return k;
+        }
+      }
+      return -1;
+    };
+  }
+
+  if (!Array.prototype.every) {
+    Array.prototype.every = function(callbackfn, thisArg) {
+      'use strict';
+      var T, k;
+
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+
+      // 1. Let O be the result of calling ToObject passing the this 
+      //    value as the argument.
+      var O = Object(this);
+
+      // 2. Let lenValue be the result of calling the Get internal method
+      //    of O with the argument "length".
+      // 3. Let len be ToUint32(lenValue).
+      var len = O.length >>> 0;
+
+      // 4. If IsCallable(callbackfn) is false, throw a TypeError exception.
+      if (typeof callbackfn !== 'function') {
+        throw new TypeError();
+      }
+
+      // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      if (arguments.length > 1) {
+        T = thisArg;
+      }
+
+      // 6. Let k be 0.
+      k = 0;
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+
+        var kValue;
+
+        // a. Let Pk be ToString(k).
+        //   This is implicit for LHS operands of the in operator
+        // b. Let kPresent be the result of calling the HasProperty internal 
+        //    method of O with argument Pk.
+        //   This step can be combined with c
+        // c. If kPresent is true, then
+        if (k in O) {
+
+          // i. Let kValue be the result of calling the Get internal method
+          //    of O with argument Pk.
+          kValue = O[k];
+
+          // ii. Let testResult be the result of calling the Call internal method
+          //     of callbackfn with T as the this value and argument list 
+          //     containing kValue, k, and O.
+          var testResult = callbackfn.call(T, kValue, k, O);
+
+          // iii. If ToBoolean(testResult) is false, return false.
+          if (!testResult) {
+            return false;
+          }
+        }
+        k++;
+      }
+      return true;
+    };
+  }
+
+  if (!Array.prototype.some) {
+    Array.prototype.some = function(fun/*, thisArg*/) {
+      'use strict';
+
+      if (this == null) {
+        throw new TypeError('Array.prototype.some called on null or undefined');
+      }
+
+      if (typeof fun !== 'function') {
+        throw new TypeError();
+      }
+
+      var t = Object(this);
+      var len = t.length >>> 0;
+
+      var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+      for (var i = 0; i < len; i++) {
+        if (i in t && fun.call(thisArg, t[i], i, t)) {
+          return true;
+        }
+      }
+
+      return false;
+    };
+  }
+
+  if (!Array.prototype.forEach) {
+
+    Array.prototype.forEach = function(callback, thisArg) {
+
+      var T, k;
+
+      if (this == null) {
+        throw new TypeError(' this is null or not defined');
+      }
+
+      // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
+      var O = Object(this);
+
+      // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
+      // 3. Let len be ToUint32(lenValue).
+      var len = O.length >>> 0;
+
+      // 4. If IsCallable(callback) is false, throw a TypeError exception.
+      // See: http://es5.github.com/#x9.11
+      if (typeof callback !== "function") {
+        throw new TypeError(callback + ' is not a function');
+      }
+
+      // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      if (arguments.length > 1) {
+        T = thisArg;
+      }
+
+      // 6. Let k be 0
+      k = 0;
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+
+        var kValue;
+
+        // a. Let Pk be ToString(k).
+        //   This is implicit for LHS operands of the in operator
+        // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
+        //   This step can be combined with c
+        // c. If kPresent is true, then
+        if (k in O) {
+
+          // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
+          kValue = O[k];
+
+          // ii. Call the Call internal method of callback with T as the this value and
+          // argument list containing kValue, k, and O.
+          callback.call(T, kValue, k, O);
+        }
+        // d. Increase k by 1.
+        k++;
+      }
+      // 8. return undefined
+    };
+  }
+
+  if (!Array.prototype.map) {
+
+    Array.prototype.map = function(callback, thisArg) {
+
+      var T, A, k;
+
+      if (this == null) {
+        throw new TypeError(' this is null or not defined');
+      }
+
+      // 1. Let O be the result of calling ToObject passing the |this| 
+      //    value as the argument.
+      var O = Object(this);
+
+      // 2. Let lenValue be the result of calling the Get internal 
+      //    method of O with the argument "length".
+      // 3. Let len be ToUint32(lenValue).
+      var len = O.length >>> 0;
+
+      // 4. If IsCallable(callback) is false, throw a TypeError exception.
+      // See: http://es5.github.com/#x9.11
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+      }
+
+      // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      if (arguments.length > 1) {
+        T = thisArg;
+      }
+
+      // 6. Let A be a new array created as if by the expression new Array(len) 
+      //    where Array is the standard built-in constructor with that name and 
+      //    len is the value of len.
+      A = new Array(len);
+
+      // 7. Let k be 0
+      k = 0;
+
+      // 8. Repeat, while k < len
+      while (k < len) {
+
+        var kValue, mappedValue;
+
+        // a. Let Pk be ToString(k).
+        //   This is implicit for LHS operands of the in operator
+        // b. Let kPresent be the result of calling the HasProperty internal 
+        //    method of O with argument Pk.
+        //   This step can be combined with c
+        // c. If kPresent is true, then
+        if (k in O) {
+
+          // i. Let kValue be the result of calling the Get internal 
+          //    method of O with argument Pk.
+          kValue = O[k];
+
+          // ii. Let mappedValue be the result of calling the Call internal 
+          //     method of callback with T as the this value and argument 
+          //     list containing kValue, k, and O.
+          mappedValue = callback.call(T, kValue, k, O);
+
+          // iii. Call the DefineOwnProperty internal method of A with arguments
+          // Pk, Property Descriptor
+          // { Value: mappedValue,
+          //   Writable: true,
+          //   Enumerable: true,
+          //   Configurable: true },
+          // and false.
+
+          // In browsers that support Object.defineProperty, use the following:
+          // Object.defineProperty(A, k, {
+          //   value: mappedValue,
+          //   writable: true,
+          //   enumerable: true,
+          //   configurable: true
+          // });
+
+          // For best browser support, use the following:
+          A[k] = mappedValue;
+        }
+        // d. Increase k by 1.
+        k++;
+      }
+
+      // 9. return A
+      return A;
+    };
+  }
+
+  if (!Array.prototype.filter) {
+    Array.prototype.filter = function(fun/*, thisArg*/) {
+      'use strict';
+
+      if (this === void 0 || this === null) {
+        throw new TypeError();
+      }
+
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (typeof fun !== 'function') {
+        throw new TypeError();
+      }
+
+      var res = [];
+      var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+      for (var i = 0; i < len; i++) {
+        if (i in t) {
+          var val = t[i];
+
+          // NOTE: Technically this should Object.defineProperty at
+          //       the next index, as push can be affected by
+          //       properties on Object.prototype and Array.prototype.
+          //       But that method's new, and collisions should be
+          //       rare, so use the more-compatible alternative.
+          if (fun.call(thisArg, val, i, t)) {
+            res.push(val);
+          }
+        }
+      }
+
+      return res;
+    };
+  }
+
+  if (!Array.prototype.reduce) {
+    Array.prototype.reduce = function(callback /*, initialValue*/) {
+      'use strict';
+      if (this == null) {
+        throw new TypeError('Array.prototype.reduce called on null or undefined');
+      }
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+      }
+      var t = Object(this), len = t.length >>> 0, k = 0, value;
+      if (arguments.length == 2) {
+        value = arguments[1];
+      } else {
+        while (k < len && !(k in t)) {
+          k++; 
+        }
+        if (k >= len) {
+          throw new TypeError('Reduce of empty array with no initial value');
+        }
+        value = t[k++];
+      }
+      for (; k < len; k++) {
+        if (k in t) {
+          value = callback(value, t[k], k, t);
+        }
+      }
+      return value;
+    };
+  }
+
+  if ('function' !== typeof Array.prototype.reduceRight) {
+    Array.prototype.reduceRight = function(callback /*, initialValue*/) {
+      'use strict';
+      if (null === this || 'undefined' === typeof this) {
+        throw new TypeError('Array.prototype.reduce called on null or undefined' );
+      }
+      if ('function' !== typeof callback) {
+        throw new TypeError(callback + ' is not a function');
+      }
+      var t = Object(this), len = t.length >>> 0, k = len - 1, value;
+      if (arguments.length >= 2) {
+        value = arguments[1];
+      } else {
+        while (k >= 0 && !(k in t)) {
+          k--;
+        }
+        if (k < 0) {
+          throw new TypeError('Reduce of empty array with no initial value');
+        }
+        value = t[k--];
+      }
+      for (; k >= 0; k--) {
+        if (k in t) {
+          value = callback(value, t[k], k, t);
+        }
+      }
+      return value;
+    };
+  }
+
+})();
